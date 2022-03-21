f8cf7ad671b56d7f9cd981bdb1addbcf3a9f1f3d
jiahonglai <522633387@qq.com>
2022-03-21 15:53:14 +0800

    更新

--- Flaskweb/uTools.py
+++ Flaskweb/uTools.py
@@ -1,16 +1,23 @@
 def readLines(filename):
     with open(filename, mode='r', encoding='utf-8') as f:
         lines = f.readlines()
-    return list(map(lambda x: x.replace('\n',''), lines))
+    return list(map(lambda x: x.replace('\n', ''), lines))
+
 
 import random
 
+
 def generateRandomTarget():
-    a, b, c, d = random.randint(1,254), random.randint(0,254), random.randint(0,254), random.randint(1,254)
+    a, b, c, d = random.randint(1,
+                                254), random.randint(0, 254), random.randint(
+                                    0, 254), random.randint(1, 254)
     return str(a) + '.' + str(b) + '.' + str(c) + '.' + str(d)
 
+
 import json
+
+
 def appendToFile(line, filename):
     line = json.dumps(line)
     with open(filename, mode='a', encoding='utf-8') as output:
-        output.write(line+'\n')
+        output.write(line + '\n')
